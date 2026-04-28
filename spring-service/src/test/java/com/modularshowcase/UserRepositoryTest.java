35aafa840d6ada9270486788200cbd4f1e2ea8a1
rushmanthnalluri <rushmanth21@gmail.com>
2026-04-28 23:04:05 +0530

    commit 194

--- spring-service/src/test/java/com/modularshowcase/UserRepositoryTest.java
+++ spring-service/src/test/java/com/modularshowcase/UserRepositoryTest.java
@@ -1,14 +1,16 @@
 package com.modularshowcase;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import com.modularshowcase.model.UserEntity;
 import com.modularshowcase.repository.UserRepository;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserRepositoryTest {
 
     @Autowired
